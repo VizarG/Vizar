e12235df27de15f5e1300db6a6e188f4dce9e5a2
VizarG <33232801+VizarG@users.noreply.github.com>
2018-11-12 16:38:58 +0300

    sql
    for sql lab 2

---
+++ notinvoke.ps1
@@ -0,0 +1,39 @@
+﻿
+
+
+Invoke-Sqlcmd -ServerInstance 169.254.17.117 -Username 'sa' -Password $a   -Query "
+
+
+USE master;  
+GO  
+IF EXISTS(select * from sys.databases where name= 'HumanResources' or  name= 'InternetSales') 
+
+	DROP DATABASE HumanResources, InternetSales
+
+
+
+ELSE
+
+ 
+	CREATE DATABASE HumanResources  
+ON  PRIMARY  
+(NAME = 'HumanResources', FILENAME = 'E:\Data\HumanResources.mdf', SIZE = 50MB, FILEGROWTH = 5MB, MAXSIZE =  UNLIMITED   )
+ 
+LOG ON  
+(NAME = 'HumanResources_log', FILENAME = 'E:\Logs\HumanResources.ldf', SIZE = 5MB , FILEGROWTH = 1MB, MAXSIZE =  UNLIMITED   );
+
+CREATE DATABASE InternetSales 
+ON  PRIMARY  
+(NAME = 'InternetSales', FILENAME = 'E:\Data\InternetSales.mdf', SIZE = 5MB, FILEGROWTH = 1MB, MAXSIZE =  UNLIMITED   ),
+
+FILEGROUP SalesData
+(NAME = 'InternetSales_data1', FILENAME = 'E:\Data\InternetSales_data1.ndf', SIZE = 100MB, FILEGROWTH = 10MB, MAXSIZE =  UNLIMITED   ),
+
+(NAME = 'InternetSales_data22', FILENAME = 'E:\AdditionalData\InternetSales_data2.ndf', SIZE = 100MB, FILEGROWTH = 10MB, MAXSIZE =  UNLIMITED   )   
+
+LOG ON  
+(NAME = 'InternetSales_log', FILENAME = 'E:\Logs\InternetSales.ldf', SIZE = 2MB , FILEGROWTH = 200KB, MAXSIZE =  UNLIMITED   );
+
+"
+
+
---
+++ sql for the first task lab2.ps1
@@ -0,0 +1,36 @@
+﻿
+
+
+
+Invoke-Sqlcmd -ServerInstance 169.254.17.117 -Username 'sa' -Password $a -Query " 
+
+SELECT name, physical_name,size,max_size,growth  
+FROM sys.master_files  
+WHERE database_id = DB_ID(N'tempdb'); 
+go
+
+USE master;  
+GO  
+ALTER DATABASE tempdb   
+MODIFY FILE (NAME = tempdev, size=10240kb , filegrowth=5120kb);  
+GO  
+ALTER DATABASE tempdb   
+MODIFY FILE (NAME = templog,  size=10240kb , filegrowth=1024kb);  
+GO  
+ALTER DATABASE tempdb   
+MODIFY FILE (NAME = tempdev, FILENAME = 'E:\tempdb.mdf');  
+GO  
+ALTER DATABASE tempdb   
+MODIFY FILE (NAME = templog, FILENAME = 'E:\tempdb.ldf');  
+GO  
+
+
+
+SELECT name, physical_name,size,max_size,growth  
+FROM sys.master_files  
+WHERE database_id = DB_ID(N'tempdb'); 
+go
+
+"
+
+
